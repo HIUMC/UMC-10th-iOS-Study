4703a2c1e4650cb03d848fcbd2c30ae0f73ca852
21g <leehan0311@gmail.com>
2026-03-18 12:07:02 +0900

    chore: 폰트, 색상 선언형으로 정의

---
+++ leehan/UMC_Megabox/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  leehan
+//
+//  Created by 이한결 on 3/18/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ leehan/UMC_Megabox/Views/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  leehan
+//
+//  Created by 이한결 on 3/18/26.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
