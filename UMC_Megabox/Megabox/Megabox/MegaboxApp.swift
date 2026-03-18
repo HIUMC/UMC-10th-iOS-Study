a1b0154960c0fb462d7557b3ac2d9e13d74546eb
chanhyeok <cksrowldms9@naver.com>
2026-03-18 14:23:16 +0900

    메가박스 과제 프로젝트 추가

---
+++ UMC_Megabox/Megabox/Megabox/MegaboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegaboxApp.swift
+//  Megabox
+//
+//  Created by chanhyeok on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MegaboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
