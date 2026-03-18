af4a4cd3118c6f010b2d2b8a9322503717dcd69b
21g <leehan0311@gmail.com>
2026-03-18 14:09:33 +0900

    feat: 스플래쉬뷰, 로그인뷰 구현

--- leehan/UMC_Megabox/Views/LoginView.swift
+++ leehan/UMC_Megabox/Views/LoginView.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var id = ""
+    @State private var pw = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("로그인")
+                .font(.PretendardSemiBold(size: 24))
+                .foregroundStyle(.black)
+            
+            Spacer()
+            
+            TextField("아이디", text: $id)
+            Divider().padding(.bottom, 40)
+            TextField("비밀번호", text: $pw)
+            Divider()
+            
+            Spacer().frame(height: 65)
+            
+            Button( action: {} ) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(.purple03)
+                        
+                    Text("로그인")
+                        .foregroundStyle(.white)
+                        .font(.PretendardBold(size: 18))
+                }
+            }.frame(height: 54)
+            
+            Spacer().frame(height: 7)
+            
+            Button( action: {} ) {
+                Text("회원가입")
+                    .foregroundStyle(.gray04)
+                    .font(.PretendardMedium(size: 13))
+            }
+            
+            Spacer().frame(height: 25)
+            
+            HStack {
+                Spacer()
+                Button( action: {} ) {
+                    Image("icon_naver")
+                }
+                Spacer()
+                Button( action: {} ) {
+                    Image("icon_kakao")
+                }
+                Spacer()
+                Button( action: {} ) {
+                    Image("icon_apple")
+                }
+                Spacer()
+            }
+            
+            Spacer().frame(height: 29)
+            
+            Image("banner_umc10th")
+                .resizable()
+                .scaledToFit()
+                
+            
+            Spacer().frame(height: 210)
+        }.padding(.horizontal)
     }
 }
 
--- leehan/UMC_Megabox/Views/SplashView.swift
+++ leehan/UMC_Megabox/Views/SplashView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        /// ZStack을 이용해 배경에 하얀 Rectangle을 넣어주고,
+        /// ignoreSafeArea 수정자를 이용해 Dark mode에서도 변화 없게 구현
+        ZStack {
+            Rectangle()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .foregroundStyle(.white)
+                .ignoresSafeArea()
+            
+            Image("logo_megabox")
+                .scaledToFit()
+        }
     }
 }
 
