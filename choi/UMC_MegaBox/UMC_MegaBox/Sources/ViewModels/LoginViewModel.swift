80d3eeca98359b991339374e1e805403c58c56e5
MinHeokChoi <min2heuk@naver.com>
2026-03-25 15:17:01 +0900

    feat: MyPageView Ui 구현

---
+++ choi/UMC_MegaBox/UMC_MegaBox/Sources/ViewModels/LoginViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewModel.swift
+//  UMC_MegaBox
+//
+//  Created by 최민혁 on 3/25/26.
+//
+
+
+import Foundation
+import SwiftUI
+
+@Observable
+class LoginViewModel {
+    var loginModel = LoginModel()
+
+}
+
