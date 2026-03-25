8833173f01e3dca961bdc50c167b7317b4bea638
김민지 <juniorandme12@gmail.com>
2026-03-25 18:34:14 +0900

    Initial Commit

---
+++ Week1_prac/Week1_pracApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week1_pracApp.swift
+//  Week1_prac
+//
+//  Created by 김민지 on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Week1_pracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TicketView()
+        }
+    }
+}
